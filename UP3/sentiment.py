b156d1ed983724016b3f4e788e0a83c4b73e7dfd
wMc126 <mw6088@nyu.edu>
2026-05-09 21:14:23 +0800

    Update sentiment.py

--- UP3/sentiment.py
+++ UP3/sentiment.py
@@ -1,18 +1,47 @@
-import subprocess
-import sys
+import re
 
-from textblob import TextBlob
+# 正面词（强度）
+POS_WORDS = {
+    "good": 1, "great": 2, "excellent": 3, "amazing": 3, "wonderful": 2,
+    "love": 2, "happy": 1, "nice": 1, "fantastic": 3, "awesome": 2,
+}
 
+# 负面词（强度）
+NEG_WORDS = {
+    "bad": 1, "terrible": 3, "awful": 3, "horrible": 3, "sad": 1,
+    "hate": 2, "annoying": 1, "useless": 2,
+}
 
-def analyze_sentiment(text):
+# 否定词
+NEGATORS = {"not", "no", "never", "isn't", "aren't", "wasn't"}
 
-    polarity = TextBlob(text).sentiment.polarity
-    if polarity > 0:
-        return "😊 [P+]"
-    elif polarity < 0:
-        return "😡 [N-]"
+def analyze_sentiment(text: str):
+    text = text.lower()
+    words = re.findall(r"\b[\w']+\b", text)
+
+    score = 0
+    negate = False
+
+    for w in words:
+        if w in NEGATORS:
+            negate = True
+            continue
+
+        val = 0
+        if w in POS_WORDS:
+            val = POS_WORDS[w]
+        elif w in NEG_WORDS:
+            val = -NEG_WORDS[w]
+
+        if negate:
+            val = -val
+            negate = False
+
+        score += val
+
+    if score > 0:
+        return "Positive 😊"
+    elif score < 0:
+        return "Negative 😡"
     else:
-        return "😐 [N]"
-
-
-
+        return "Neutral 😐"
